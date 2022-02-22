7b6d990041ee482a9250b77344638babfa1a253e
LuxeZN <zn475921@ohio.edu>
2022-02-21 21:23:12 -0500

    Solution for LeetCode 1929 Concatenation of Array

---
+++ leetcode1929.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = [0] * (2 * len(nums))
+        i = 0
+        while (i < len(nums)):
+            ans[i] = nums[i]
+            ans[i + len(nums)] = nums[i]
+            i += 1
+        return ans
+            
