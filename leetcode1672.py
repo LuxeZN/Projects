459cd7b32980c14b5ecac4e542cae10b44767c75
LuxeZN <zn475921@ohio.edu>
2022-02-28 19:17:49 -0500

    LeetCode 1672. Richest Customer Wealth solution

---
+++ leetcode1672.py
@@ -0,0 +1,20 @@
+#Author: Zach Naymik
+#Date: February 28, 2022
+
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        max_total = 0
+        for i in accounts:
+            total = 0
+            for j in i:
+                total += j
+            if (total > max_total):
+                max_total = total
+        return max_total
+            
+        
+        
